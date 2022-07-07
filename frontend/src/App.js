8292961a30d4a1f1b5fd3e52d451f3ae05925cf9
jlxw48 <jlxw48@gmail.com>
2022-07-07 17:42:26 +0800

    best commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { KendraClient, QueryCommand } from "@aws-sdk/client-kendra"; 
+import { useEffect } from "react";
+
+async function hi() {
+  console.log('hi');
+    const cred = {
+      accessKeyId: "AKIAU5LMWJ3PGZE7XV5L",
+      secretAccessKey: "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",
+    };
+    const client = new KendraClient({
+      credentials: cred,
+      region: "us-west-2"
+    });
+    const input = {
+      IndexId: "58426b77-30be-4b78-8240-13017bb7f40c", 
+      QueryText: "kafka",
+      PageNumber: 1
+    };
+    const command = new QueryCommand(input);
+    const response = await client.send(command);
+    console.log(response);
+}
 
 function App() {
-  return (
+  // a client can be shared by different commands.
+
+  useEffect(() => {
+    const bye = async () => {
+      await hi();
+    }
+
+    bye().catch(console.error);
+  }, []
+  )
+
+  return (  
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
